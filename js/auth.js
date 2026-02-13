5c740665d32828dfdb46f08ddc2ea3f15648e47d
HYOUZII <cokeneon@gmail.com>
2026-02-13 20:38:37 +0900

    App script url 배포 문제 재시도

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,7 @@
 // 연구소 업무 관리 대시보드 - 인증 관리 모듈
 
 // ⚠️ 여기에 본인의 Apps Script URL을 입력하세요
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby3Fw1OvC1nSyQAJiQuRgdkGe01ca-c-JL64y_s6f4l0QWxMfgOa5WRqSi5-9FSZH8/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw0Ee12KPlLkae9qrYqKmTmbbG9KkTLQ6rtUUoYTxLtym_DCRGE3tuqu9fERwQTbtY/exec';
 
 // 세션 유효 시간 (시간)
 const SESSION_DURATION_HOURS = 8;
